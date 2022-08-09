d0c5b5ab8f0b19d205478fe342650ba5ac5bf5d5
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-08-09 16:41:03 +0200

    user model autenticacion

--- src/api/user/user.model.js
+++ src/api/user/user.model.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bcryp = require('bcrypt');
+const {validatePassword, setError} = require ('../../helpers/utils');
+
 
 const schema = new Schema({
     img: { type: String, unique: true, required: true },
@@ -16,5 +18,15 @@ const schema = new Schema({
         timestamps: true
     }
 );
+
+schema.pre('save', function (next){
+    if (!validatePassword(this.password)) return next (setError('400','Contraseña Invalida '))
+    this.password =bcryp.hashSync(this.password, 16);
+    next();
+
+
+
+});
+
 
 module.exports = mongoose.model('user', schema);
